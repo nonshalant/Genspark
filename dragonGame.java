e861d4091a2b579c657f2d4a95ddb622de78b8ba
Shamir <Shamir.gibbons@gmail.com>
2022-01-31 13:02:29 -0500

    The dragon game edit

---
+++ dragonGame.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class dragonGame {
+    public static void main(String[] args) {
+
+        System.out.println("You are in a land full of dragons. In front of you, you see two caves." +
+                " In one cave, the dragon is friendly and will share his treasure with you." +
+                "The other dragon is greedy and hungry and will eat you on sight. " +
+                "Which Cave will you go in (0 , 1, 2)");
+
+        Scanner getInput = new Scanner(System.in);
+        int input = getInput.nextInt();
+
+        if (input == 1) {
+            System.out.println("You entered cave 1 and have been eaten by the dragon! Would you like to play again? (yes or no)");
+        } else if (input == 2) {
+            System.out.println("You entered cave 2 CONGRATS!! YOU'RE RICH! :D  Would you like to play again? (yes or no)");
+        } else if (input == 0) {
+            System.out.println(" Yopu exited the cave. Would you like to play again? (yes or no)");
+        }
+    }
+}
